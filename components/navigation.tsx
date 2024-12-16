1f937120c3619556995f5724f9ab90fd2e4fec06
Owen <dhfma0713@gmail.com>
2024-12-16 13:14:36 +0900

    nextupdata

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,12 +3,12 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-
+import styles from "../styles/navigation.module.css";
 export default function Navigation() {
   const path = usePathname();
   const [count, setCount] = useState(0);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
